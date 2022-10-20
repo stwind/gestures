e21443eeb6f3750536eea0428ad4db14e3dbab61
stwind <stwindfy@gmail.com>
2022-10-20 19:23:20 +0800

    add signals

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import { render } from "preact";
+import { useSignal } from "@preact/signals";
 import "./index.css";
 
 const App = () => {
-  return <div>hello</div>;
+  const count = useSignal(0);
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => count.value++}>+</button>
+    </>
+  );
 };
 
 render(<App />, document.getElementById("app") as HTMLElement);
