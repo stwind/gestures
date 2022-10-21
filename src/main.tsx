8e82160d7b478842a87fb840afa1e3efda6a92d5
stwind <stwindfy@gmail.com>
2022-10-21 11:53:15 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,56 @@
-import { render } from "preact";
-import { useSignal } from "@preact/signals";
+import { type FunctionComponent, render } from "preact";
+import { effect, Signal, signal } from "@preact/signals";
 import "./index.css";
 
-const App = () => {
-  const count = useSignal(0);
-  return (
-    <>
-      <div>{count}</div>
-      <button onClick={() => count.value++}>+</button>
-    </>
-  );
+// Utils
+
+const raf = (f: (t: DOMHighResTimeStamp) => any) => {
+  let handle = requestAnimationFrame(function frame(t) {
+    if (f(t) !== false) handle = requestAnimationFrame(frame);
+  });
+  return () => cancelAnimationFrame(handle);
 };
 
-render(<App />, document.getElementById("app") as HTMLElement);
+// State
+
+interface State {
+  frame: Signal<number>;
+  paused: Signal<boolean>;
+}
+
+const toggle = ({ frame, paused }: State) => {
+  let cancel: () => void;
+  return effect(() => {
+    if (paused.value) {
+      if (cancel) cancel();
+    } else {
+      cancel = raf(() => frame.value++);
+    }
+  });
+};
+
+const state: State = {
+  frame: signal(0),
+  paused: signal(false),
+};
+
+toggle(state);
+
+// UI
+
+const App: FunctionComponent<{ state: State }> = ({
+  state: { frame, paused },
+}) => (
+  <>
+    <div>{frame}</div>
+    <button
+      style={{ width: "3.5rem" }}
+      onClick={() => (paused.value = !paused.value)}
+    >
+      {paused.value ? "run" : "pause"}
+    </button>
+    <button onClick={() => (frame.value = 0)}>reset</button>
+  </>
+);
+
+render(<App state={state} />, document.getElementById("app") as HTMLElement);
